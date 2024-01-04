b371a8ed1987e14ae54bc1f9ecd0d1a593f94c3d
哇呀 <2364174415@qq.com>
2024-01-04 18:06:26 +0800

    finish

---
+++ src/build/mapcloud/CMakeFiles/mapcloud_node.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/fishros/ros2_ws/src/mapcloud/src/mapcloud.cpp" "CMakeFiles/mapcloud_node.dir/src/mapcloud.cpp.o" "gcc" "CMakeFiles/mapcloud_node.dir/src/mapcloud.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/mapcloud/CMakeFiles/mapcloud_node.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mapcloud_node.dir/src/mapcloud.cpp.o"
+  "CMakeFiles/mapcloud_node.dir/src/mapcloud.cpp.o.d"
+  "mapcloud_node"
+  "mapcloud_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mapcloud_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
